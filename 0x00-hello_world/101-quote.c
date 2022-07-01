bd5b395bafd9fa4bc7a92416252e825bd84babf0
MichelEloka <elokamichel@gmail.com>
2022-07-01 09:01:57 -0700

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,14 @@
-e File  11 lines (11 sloc)  237 Bytes
-#include<unistd.h>
+#include <stdio.h>
+
+#include <unistd.h>
+
 /**
- *
- *  * main - prints the standrad error sandwiched around the  standard outpu/t
- *
- *   *
- *
- *    * Return: returns 1
- *
- *     */
+ *main - Entry point
+ *Return:1
+ */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, ch, sizeof(ch));
 	return (1);
 }
